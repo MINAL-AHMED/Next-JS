d71039065fc3903c859753d8c68be1f02e80e11f
Minal Ahmed <mimalahmed6219@gmail.com>
2024-03-18 22:14:39 +0600

    Nested Layouts implements products page

---
+++ route-demo/app/products/[productId]/layout.tsx
@@ -0,0 +1,11 @@
+export default function ProductsDetailsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      {children};<h1>Features Product</h1>
+    </>
+  );
+}
