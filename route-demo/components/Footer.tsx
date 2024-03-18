edd9c42c4a1aba2e7b07e12335f70963135665cd
Minal Ahmed <mimalahmed6219@gmail.com>
2024-03-18 22:04:20 +0600

    learn layout system

---
+++ route-demo/components/Footer.tsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        backgroundColor: "#333",
+        color: "#fff",
+        textAlign: "center",
+        padding: "1rem",
+        position: "fixed",
+        left: "0",
+        bottom: "0",
+        width: "100%",
+      }}
+    >
+      <p>© 2024 Your Website Name</p>
+    </footer>
+  );
+};
+
+export default Footer;
