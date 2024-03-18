6c6bca72fbf2326088a67e9dea7efa94a70b9ce2
Minal Ahmed <mimalahmed6219@gmail.com>
2024-03-18 22:27:35 +0600

    add route groupe layout

---
+++ route-demo/app/(auth)/(with-auth-layout)/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <h1>Inner Layout</h1>
+      {children}
+    </div>
+  );
+}
