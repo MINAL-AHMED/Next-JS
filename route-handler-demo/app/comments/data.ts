38efebe9ffde8636f608198ce930ebbc377e653f
Minal Ahmed <mimalahmed6219@gmail.com>
2024-03-19 16:02:10 +0600

    create api routing get and post request

---
+++ route-handler-demo/app/comments/data.ts
@@ -0,0 +1,14 @@
+export const comments = [
+  {
+    id: 1,
+    text: "This is a comment first Comment",
+  },
+  {
+    id: 2,
+    text: "This is a comment second Comment",
+  },
+  {
+    id: 3,
+    text: "This is a comment third Comment",
+  },
+];
